7af624957aea7614a91230e711b3b40bd4559806
CGM <mrmouse.guru@gmail.com>
2024-03-19 13:35:56 +0700

    create a web http server using http moudle nodejs

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import http from "http"
 
 let server = http.createServer();
 
+
+//default: GET
+const handleRequest = (req, res) =>{
+    console.log(`Got a request for ${req.method} ${req.url}`);
+};
+
+server.on("request", handleRequest);
+
 server.listen(9999, () =>{
     console.log("Now listening on port 9999 ....");
 });
